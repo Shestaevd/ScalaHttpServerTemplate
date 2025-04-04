bf2a867e63f1c5221f5c021a6d77a4fa49da7015
Dmitriy <shestaevd26@gmail.com>
2025-04-04 20:37:24 +0400

    request example

--- src/main/scala/http/UnusedHints.scala
+++ src/main/scala/http/UnusedHints.scala
@@ -1,12 +1,17 @@
 package org.pvgus
 package http
 
+import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.model.{HttpEntity, HttpRequest, HttpResponse}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
+import org.pvgus.http.Server.system
 import spray.json.DefaultJsonProtocol._
 import spray.json.{DefaultJsonProtocol, RootJsonFormat}
+
+import scala.concurrent.Future
 
 object UnusedHints {
 
@@ -20,5 +25,12 @@ object UnusedHints {
       }
     }
   }
+
+  val requestExample: Future[HttpResponse] = Http.singleRequest(
+    HttpRequest(
+      uri = "http://akka.io",
+      entity = HttpEntity("{}")
+    )
+  )
 
 }
